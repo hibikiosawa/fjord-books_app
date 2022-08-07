5806d5d4a1db49b9a0a24566fb9816cc75b09c42
osawa <hibiki20000627@gmail.com>
2022-08-08 01:39:02 +0900

    letter openerの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get 'users', to: 'users#index'
   get 'users/:id', to: 'users#show'
   resources :books
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
