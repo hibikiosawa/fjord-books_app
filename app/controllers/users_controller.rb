ed91a8cc1288e01b32b11072956b9add70e83b05
osawa <hibiki20000627@gmail.com>
2022-08-08 00:49:02 +0900

    翻訳ファイルの追加等

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
+  def index
+    @users = User.order(:id).page(params[:page])
+  end
+
   def show
   end
   
