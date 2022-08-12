7cf75e0a20b680e33b4f392a49c229a56f7880b7
osawa <hibiki20000627@gmail.com>
2022-08-12 15:59:35 +0900

    不要なファイルの削除

--- app/models/comment.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Comment < ApplicationRecord
-  has_one_attached :avatar
-end
--- db/schema.rb
+++ db/schema.rb
@@ -49,12 +49,6 @@ ActiveRecord::Schema.define(version: 2022_08_09_053117) do
     t.string "picture"
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
