8584688b1b4fc99522f3ac1d629b22e87b52b791
osawa <hibiki2_627@yahoo.co.jp>
2022-08-17 15:33:51 +0900

    routesの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'users', to: 'users#index'
-  get 'users/:id', to: 'users#show'
-  resources :books
+  resources :users, :books
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
