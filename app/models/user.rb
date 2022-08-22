2f258d8aa1a547fc06d20c574794866670cf7bac
osawa <hibiki2_627@yahoo.co.jp>
2022-08-22 10:14:59 +0900

    フォロー、フォロー解除機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,27 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   has_one_attached :avatar
+
+  # フォローをした、されたの関係
+  has_many :followfollowers, class_name: "Followfollower", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_followfollowers, class_name: "Followfollower", foreign_key: "followed_id", dependent: :destroy
+
+  # 一覧画面で使う
+  has_many :followings, through: :followfollowers, source: :followed
+  has_many :followers, through: :reverse_of_followfollowers, source: :follower
+
+  # フォローしたときの処理
+def follow(user_id)
+  followfollowers.create(followed_id: user_id)
+end
+# フォローを外すときの処理
+def unfollow(user_id)
+  followfollowers.find_by(followed_id: user_id).destroy
+end
+# フォローしているか判定
+def following?(user)
+  followings.include?(user)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
+
+  get 'followfollowers/followings'
+  get 'followfollowers/followers'
+
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books
   resources :users, only: %i(index show)
+
+  # ネストさせる
+  resources :users do
+    resource :followfollowers, only: [:create, :destroy]
+    get 'followings' => 'followfollowers#followings', as: 'followings'
+    get 'followers' => 'followfollowers#followers', as: 'followers'
+
+  end
 end
