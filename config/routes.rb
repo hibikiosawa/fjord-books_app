c8f128155ede34d29e9ecf5b90c1f8ee65c3bac0
osawa <hibiki2_627@yahoo.co.jp>
2022-08-22 12:27:23 +0900

    erblintの対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'followfollowers/followings'
-  get 'followfollowers/followers'
-
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books
