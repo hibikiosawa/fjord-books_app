cadedef66ed30ff70008f1a1f7abc8c58c2f3904
osawa <hibiki2_627@yahoo.co.jp>
2022-08-23 16:57:02 +0900

    日報登録後リダイレクトするようにする

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,13 +4,19 @@ class ReportsController < ApplicationController
     @report = Report.new
   end
 
+  def show
+    @reports = Report.find(params[:id])
+  end
+
   def index
     @reports = Report.order(:id).page(params[:page])
   end
 
   def create
     @report = Report.new(report_params)
+    @report.user_id = current_user.id
     @report.save
+    redirect_to reports_path
   end
 
   def report_params
