337390351f305baaa8310f1acc79d6d50a60adf7
osawa <hibiki2_627@yahoo.co.jp>
2022-08-25 14:56:01 +0900

    日報編集機能の追加

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,5 @@
 class ReportsController < ApplicationController
+  before_action :set_report, only: %i[show edit update destroy]
   
   def new
     @report = Report.new
@@ -11,17 +12,34 @@ class ReportsController < ApplicationController
   def create
     @report = Report.new(report_params)
     @report.user_id = current_user.id
-    @report.save
-    redirect_to reports_path
+    if @report.save
+      redirect_to reports_url
+    else
+      render :reports_path
+    end
   end
 
-  def show
-    @report = Report.find(params[:id])
-  end
+  def show;  end
 
   def destroy
-    @report = Report.find(params[:id])
     @report.destroy
+    redirect_to reports_url
+  end
+
+  def update
+    if @report.update(report_params)
+      redirect_to reports_url
+    else
+      render :edit
+    end 
+  end
+
+  def edit; end
+
+  private
+
+  def set_report
+    @report = Report.find(params[:id])
   end
 
   def report_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,13 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
-  resources :reports
   
   resources :books do  
-    resources :comments, only: %i[create destroy], module: :books  
+    resources :comments, only: %i[create], module: :books  
   end  
 
   resources :reports do  
-    resources :comments, only: %i[create destroy], module: :reports 
+    resources :comments, only: %i[create], module: :reports 
   end  
 
   resources :users, only: %i[index show] do
